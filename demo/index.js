bef54772122706df3cbcc21e792af68135562ae5
Jarvan <liushileijarvan@gmail.com>
2017-08-16 14:14:09 +0800

    p

--- demo/index.js
+++ demo/index.js
@@ -45,4 +45,45 @@ var strlength2 = someValue.length;
 function identity(arg) {
     return arg;
 }
-// 这就保证了函数identity的输入参数类型和返回类型是同一个类型的 
+// 这就保证了函数identity的输入参数类型和返回类型是同一个类型的，既可以直接指定类型，也可以通过类型推断得出具体类型
+var output = identity("myString"); //let output = identity("myString");
+//泛型的数组形式
+//在打印length的时候要求arg有length属性，而number没有length，所以会报错，这里就是用T[]表示这是一个T类型的数组
+// function loggingIdentity<T>(arg: T[]): T[] {//function loggingIdentity<T> (arg:Array<T>):Array<T>{
+// 	console.log(arg.length);  // Error: T doesn't have .length
+// 	return arg;
+// }
+//泛型函数
+var fun = function (arg) {
+    return arg;
+};
+//泛型函数比普通函数多了泛型规定，即函数名字后面跟上<T>确定泛型参数(arg:T)表明参数的类型是泛型T，=>T  表名返回的是T   通过这种形式表明此函数的三点1，类型参数，2，数据参数的类型，3，函数的返回值类型
+//泛型接口
+// interface test<T>{
+// 	//这个接口规定了继承自此接口的必须要有一个方法是返回值类型和参数类型相同的！
+// 	(arg: T): T;
+// }
+//
+// function aaa<U>(arg:U):U{
+// 	return arg
+// }
+// let a4:test<number> = aaa;
+//泛型类
+var testClass = (function () {
+    function testClass() {
+    }
+    return testClass;
+}());
+var tt = new testClass();
+tt.zeroValue = 22;
+tt.add(2, 3);
+function loggingIdentity(arg) {
+    console.log(arg.length); // Error: T doesn't have .length
+    return arg;
+}
+function getProperty(obj, key) {
+    return obj[key];
+}
+var x2 = { a: 1, b: 2, c: 3, d: 4 };
+getProperty(x, "a"); // okay
+getProperty(x, "m");
